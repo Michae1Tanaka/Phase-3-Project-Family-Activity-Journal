66df5e7ad037366d6e17257be4130bc4031ecc68
Michael Tanaka <tanakam4214@gmail.com>
2023-08-14 14:16:26 -0700

    adds basic code to photo.py

--- lib/db/classes/photo.py
+++ lib/db/classes/photo.py
@@ -1,1 +1,9 @@
+from helpers import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
+
+class Photo(Base):
+    __tablename__ = "photos"
+
+    id = Column(Integer(), primary_key=True)
+    url = Column(String())
