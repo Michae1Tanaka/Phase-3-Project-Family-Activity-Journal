94bad232ccace52ae154160bdf5fccfb2cf37585
Michael Tanaka <tanakam4214@gmail.com>
2023-08-14 13:46:12 -0700

    initializes SQLAlchemy Base in helpers.py

---
+++ lib/helpers.py
@@ -0,0 +1,10 @@
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
