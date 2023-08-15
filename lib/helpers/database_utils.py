8f5ae32629c24c9f103f4b7c34a3a6574760aa6b
Michael Tanaka <tanakam4214@gmail.com>
2023-08-14 17:36:16 -0700

    adds a sessionmaker inside helpers.py; creates helpers folder; moves helpers.py inside helpers folder; renames helper.py to database_utils.py

---
+++ lib/helpers/database_utils.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+
+metadata = MetaData(naming_convention=convention)
+Base = declarative_base(metadata=metadata)
+engine = create_engine("sqlite:///")
+
+Session = sessionmaker(bind=engine)
+session = Session()
