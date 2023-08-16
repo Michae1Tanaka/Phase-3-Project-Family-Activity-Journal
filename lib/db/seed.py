b395e9cf37dd7727470b7cd248c5c290cf03706c
Michael Tanaka <tanakam4214@gmail.com>
2023-08-15 23:45:19 -0700

    adds activity seed data to table; tests: works

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -10,14 +10,17 @@ from .models.activity_category_association import activity_category
 fake = Faker()
 session = Session()
 
+weather_options = ["cloudy", "sunny", "rainy", "snowy"]
+
 
 def delete_records():
     session.query(Photo).delete()
+    session.query(Activity).delete()
 
 
 def create_photos():
     photos = []
-    for i in range(10):
+    for _ in range(10):
         photo = Photo(
             photo_description=fake.paragraph(nb_sentences=1),
             url=fake.image_url(),
@@ -29,6 +32,23 @@ def create_photos():
     return photos
 
 
+def create_activities():
+    activities = []
+    for _ in range(30):
+        activity = Activity(
+            name=fake.paragraph(nb_sentences=1),
+            description=fake.paragraph(nb_sentences=1),
+            notes=fake.paragraph(nb_sentences=1),
+            location=fake.city() + ", " + fake.state(),
+            weather=rc(weather_options),
+        )
+        session.add(activity)
+        session.commit()
+        activities.append(activity)
+    return activities
+
+
 if __name__ == "__main__":
     delete_records()
     photos = create_photos()
+    activities = create_activities()
