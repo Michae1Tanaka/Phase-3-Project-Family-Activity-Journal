fe3b9cc07e7cfd20445b5e2fdd42d1fcf9cf8b94
Michael Tanaka <tanakam4214@gmail.com>
2023-08-15 21:56:26 -0700

    adds __repr__ to Photo

--- lib/db/models/photo.py
+++ lib/db/models/photo.py
@@ -7,7 +7,15 @@ class Photo(Base):
     __tablename__ = "photos"
 
     id = Column(Integer(), primary_key=True)
+    photo_description = Column(String())
     url = Column(String())
 
     activity_id = Column(Integer(), ForeignKey("activities.id"))
     activity = relationship("Activity", back_populates="photos")
+
+    def __repr__(self):
+        return (
+            f"<Photo {self.photo_description}\n"
+            + f"<URL: {self.url}\n"
+            + f"<Activity ID: {self.activity_id}\n"
+        )
--- lib/db/seed.py
+++ lib/db/seed.py
@@ -9,13 +9,3 @@ from .models.category import Category
 from .models.activity_category_association import activity_category_association
 
 fake = Faker()
-
-if __name__ == "__main__":
-    w = Activity()
-    x = Photo()
-    y = Category
-    z = activity_category_association
-    print(w)
-    print(x)
-    print(y)
-    print(z)
