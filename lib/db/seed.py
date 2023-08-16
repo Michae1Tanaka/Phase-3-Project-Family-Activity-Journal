467a417756731f372def95ff5cf9f8ce87fc8e3b
Michael Tanaka <tanakam4214@gmail.com>
2023-08-15 22:38:36 -0700

    adds photo seed data to table; test seed.py: works

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -9,3 +9,21 @@ from .models.activity_category_association import activity_category
 
 fake = Faker()
 session = Session()
+
+
+def create_photos():
+    photos = []
+    for i in range(10):
+        photo = Photo(
+            photo_description=fake.paragraph(nb_sentences=1),
+            url=fake.image_url(),
+            activity_id=rc(range(10)),
+        )
+        session.add(photo)
+        session.commit()
+        photos.append(photo)
+    return photos
+
+
+if __name__ == "__main__":
+    photos = create_photos()
