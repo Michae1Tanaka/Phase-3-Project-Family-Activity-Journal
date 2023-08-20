d0b491b8e58f0b5f6f108ae46139980435caae54
Michael Tanaka <tanakam4214@gmail.com>
2023-08-19 17:37:24 -0700

    adds activity_menu skeleton to cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -17,32 +17,50 @@ def start():
 
 def main_menu():
     print("Main Menu:\n")
-    print(
-        "Would you like to:\n"
-        + "1:View Activities\n"
-        + "2:View Categories\n"
-        + "3:View Table"
-    )
-    menu_options = input("Type 1, 2 , 3, or exit : ")
-    if menu_options == 1:
-        # view_activities()
-        pass
-    elif menu_options == 2:
-        # view_categories()
-        pass
-    elif menu_options == 3:
-        # view_categories()
-        pass
-    elif menu_options.lower() == "exit":
-        exit()
+    print("Would you like to:\n1:View Activities\n2:View Categories\n3:View Table")
+    menu_option = input("Type 1, 2 , 3, or exit : ")
+    if menu_option.lower() == "exit":
+        exit(main_menu)
+    elif menu_option in ["1", "2", "3"]:
+        if int(menu_option) == 2:
+            # view_categories()
+            pass
+        elif int(menu_option) == 3:
+            # view_table() or view_photos()
+            pass
+        elif int(menu_option) == 1:
+            activities_menu()
     else:
-        print("Input must be 'y' for yes, 'n' for no, or exit to exit.")
-        time.sleep(3)
-        clear_terminal()
+        multi_choice_error()
         main_menu()
 
 
-def exit():
+def activities_menu():
+    clear_terminal()
+    print("Activities Menu:\n")
+    print(
+        "Would you like to:\n1: Update an activity\n2: Delete an activity\n3: View activities?"
+    )
+    activity_option = input("Type 1, 2, 3, or exit : ")
+    if activity_option.lower() == "exit":
+        exit(activities_menu)
+    elif activity_option in ["1", "2", "3"]:
+        activity_option = int(activity_option)
+        if activity_option == 2:
+            # delete_activity()
+            pass
+        elif activity_option == 3:
+            # update_activity()
+            pass
+        elif activity_option == 1:
+            # view_activities()
+            pass
+    else:
+        multi_choice_error()
+        activities_menu()
+
+
+def exit(prev_menu):
     print("Are you sure you want to exit?\n")
     exit_y_or_n = input("y/n: ")
     if exit_y_or_n == "y":
@@ -52,7 +70,7 @@ def exit():
         pass
     elif exit_y_or_n == "n":
         clear_terminal()
-        main_menu()
+        prev_menu()
     else:
         y_n_error()
         clear_terminal()
@@ -63,9 +81,18 @@ def clear_terminal():
     print("\n" * 64)
 
 
+def multi_choice_error():
+    clear_terminal()
+    print("Input must be a number that corresponds to an option or exit.")
+    time.sleep(3)
+    clear_terminal()
+
+
 def y_n_error():
+    clear_terminal()
     print("Input must be 'y' for yes or 'n' for no.")
     time.sleep(2)
+    clear_terminal()
 
 
 if __name__ == "__main__":
